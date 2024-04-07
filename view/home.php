2fed7341a82a01d9b0ad309ff6ddf1f12ea35c1a
Nana Adwoa <naelegba@gmail.com>
2024-04-07 22:29:17 +0100

    new home page

---
+++ view/home.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GlobeTrotter-Home</title>
+    <link rel="stylesheet" href="../css/home.css">
+    <!-- Font Awesome CDN link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+
+    <!--Swiper CSS link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+</head>
+<body>
+
+<header >
+        <a href="#" class="brand">GlobeTrotter</a>
+        <div class="nav">
+            <div class="nav-items">
+                <a href="../view/home.php">Home</a>
+                <a href="../view/destination.php">Destinations</a>
+                <a href="../view/hotels.php">Hotels</a>
+                <a href="../view/package.php">Packages</a>
+                <a href="../view/book.php">Book trip</a>
+                <a href="../view/budget.php">Budget calculator</a>
+            </div>
+
+        </div>
+    </header>
+
+
+
+
+
+
+    <section class="footer">
+<div class="box-container">
+
+<div class="box">
+    <h3>Quick links</h3>
+<a href="../view/home.php"> <i class="fas fa-angle-right"></i>Home</a>
+<a href="../view/destination.php"><i class="fas fa-angle-right"></i>Destinations</a>
+<a href="../view/hotels.php"><i class="fas fa-angle-right"></i>Hotels</a>
+<a href="../view/package.php"><i class="fas fa-angle-right"></i>Packages</a>
+ <a href="../view/book.php"><i class="fas fa-angle-right"></i>Book trip</a>
+ <a href="../view/budget.php"><i class="fas fa-angle-right"></i>Budget calculator</a>
+</div>
+
+</div>
+    </section>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="../js/java.js"></script>
+
+<!--Swiper js-->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+</body>
+</html>
