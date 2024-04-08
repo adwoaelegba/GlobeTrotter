d60fa2a7dbf7a4b740df7edefbbb13159451e905
Nana Adwoa <naelegba@gmail.com>
2024-04-08 03:38:21 +0100

    home statte (without font colour change)

--- view/home.php
+++ view/home.php
@@ -30,43 +30,84 @@
 
 
 
+    <!--home slider -->
+    <section class="home">
 
+    <div class="swiper home-slider">
 
+    <div class="swiper-wrapper">
 
-    <section class="footer">
-<div class="box-container">
+    <!--Fist slider-->
+    <div class="swiper-slide" style="background: url(../images/jamaica.jpg) no-repeat;">
 
-<div class="box">
-    <h3>Quick links</h3>
-<a href="../view/home.php"> <i class="fas fa-angle-right"></i>Home</a>
-<a href="../view/destination.php"><i class="fas fa-angle-right"></i>Destinations</a>
-<a href="../view/hotels.php"><i class="fas fa-angle-right"></i>Hotels</a>
-<a href="../view/package.php"><i class="fas fa-angle-right"></i>Packages</a>
- <a href="../view/book.php"><i class="fas fa-angle-right"></i>Book trip</a>
- <a href="../view/budget.php"><i class="fas fa-angle-right"></i>Budget calculator</a>
+    <div class="content">
+<span>Explore The World</span>
+<h3>We currently have some countries in each continent and division in our portfolio that we cater to.Check out which ones</h3>
+<a href="../view/destination.php" class="btn">Learn more</a>
+    </div>
+
+    </div>
+
+    <!--second-->
+    <div class="swiper-slide" style="background: url(../images/masaai.jpg) no-repeat;">
+
+<div class="content">
+<span>Don't Break The Bank</span>
+<h3>We allow you to calculate your budget so you can keep track of your spending</h3>
+<a href="../view/budget.php" class="btn">Learn more</a>
 </div>
 
 </div>
+<!--third-->
+<div class="swiper-slide" style="background: url(../images/boat.jpeg) no-repeat;">
+
+<div class="content">
+<span>Packages For Everyone</span>
+<h3>We offer packages to everyone, regardless of budget. You don't have to break the bank to travel with us</h3>
+<a href="../view/package.php" class="btn">Learn more</a>
+</div>
+
+</div>
+
+<!--last slide-->
+<div class="swiper-slide" style="background: url(../images/dubai.jpg) no-repeat;">
+
+<div class="content">
+<span>Made Your Choice?</span>
+<h3>Book that trip and get travelling</h3>
+<a href="../view/book.php" class="btn">Book Now</a>
+</div>
+
+</div>
+
+<!--next-->
+
+    </div>
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+
+    </div>
+
     </section>
-    
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-<script src="../js/java.js"></script>
 
 <!--Swiper js-->
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script>
+    var swiper = new Swiper(".home-slider", {
+        loop:true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+</script>
+
+
+
 
 </body>
 </html>
