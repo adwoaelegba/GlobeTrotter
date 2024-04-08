21766e7bc558a04c6ec6b35c340a98a311af59f4
Nana Adwoa <naelegba@gmail.com>
2024-04-08 04:58:52 +0100

    flag uploads

---
+++ view/destination.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GlobeTrotter-Destination</title>
+    <link rel="stylesheet" href="../css/home.css">
+    <!-- Font Awesome CDN link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+</head>
+<body>
+<header >
+        <a href="#" class="brand">GlobeTrotter</a>
+        <div class="nav">
+            <div class="nav-items">
+                <a href="../view/home.php">Home</a>
+                <a href="../view/destination.php">Destinations</a>
+                <a href="../view/package.php">Packages</a>
+                <a href="../view/book.php">Book trip</a>
+                <a href="../view/budget.php">Budget calculator</a>
+            </div>
+
+        </div>
+    </header>
+</body>
+</html>
