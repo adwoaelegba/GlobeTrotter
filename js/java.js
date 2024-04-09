0cac50ba3519d32d42d1c635b1a5385417ce1dea
Nana Adwoa <naelegba@gmail.com>
2024-04-09 03:29:32 +0100

    desination page without js

--- js/java.js
+++ js/java.js
@@ -29,25 +29,62 @@ function valEmail(){
     //slider
     
 var swiper = new Swiper(".cardSwiper", {
-    slidesPerView: 4,
-    spaceBetween: 10,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    slidesOffsetBefore: 0,
+    slidesOffsetAfter:0,
+    speed:800,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
-    //breakpoints: {
-    //    640: {
-     //   slidesPerView: 2,
-      //  spaceBetween: 20,
-     //   },
-       //  768: {
-       //  slidesPerView: 4,
-        // spaceBetween: 40,
-        // },
-       //  1024: {
-        // slidesPerView: 5,
-        // spaceBetween: 50,
-        // },
-       //  },
+    breakpoints: {
+        710: {
+            slidesPerView: 2,
+            slidesOffsetBefore: 30,
+        },
+        780: {
+            slidesPerView: 2.2,
+            slidesOffsetBefore: 30,
+        },
+        
+        1070: {
+            slidesPerView: 3,
+            slidesOffsetBefore: 30,
+        },
+
+        1280: {
+            slidesPerView: 3.7,
+            slidesOffsetBefore: 30,
+        },
+        1560: {
+        slidesPerView: 4.4,
+        slidesOffsetBefore: 30,
+        },
+   
+   },
 });
+
+
+
+//open modal
+
+const header=document.querySelector(".header");
+//const sliderNav= document.querySelector(".slide-nav");
+const readMore= document.querySelectorAll(".read-more-btn");
+
+ function openModal(){
+    header.classList.add("hide");
+    //sliderNav.classList.add("hide");
+   
+}
+
+readMore.forEach((readBtn) =>{
+    readBtn.addEventListener("click", openModal);
+    //readBtn.addEventListener("click",() => {
+        //openModal(i)
+    });
+    
+//});
+
     
--- view/destination.php
+++ view/destination.php
@@ -44,7 +44,7 @@
 <div class="slider-cont">
 <div class="swiper cardSwiper">
     <div class="swiper-wrapper">
-        <!--slider sart-->
+        <!--slider start-->
       <div class="swiper-slide">
         <div class="card ghana-bg">
             <div class="card-img">
@@ -61,7 +61,7 @@
                     </div>
                 </div>
                 <div class="read-more-btn">
-                    <a><span>Read more</span></a>
+                    <a onclick="openPopup()"><span>Read more</span></a>
                 </div>
             </div>
 
@@ -85,7 +85,7 @@
                     </div>
                 </div>
                 <div class="read-more-btn">
-                    <a><span>Read more</span></a>
+                    <a onclick="openPopup()"><span>Read more</span></a>
                 </div>
             </div>
 
@@ -109,7 +109,7 @@
                     </div>
                 </div>
                 <div class="read-more-btn">
-                    <a><span>Read more</span></a>
+                    <a onclick="openPopup()"><span >Read more</span></a>
                 </div>
             </div>
 
@@ -133,7 +133,7 @@
                     </div>
                 </div>
                 <div class="read-more-btn">
-                    <a><span>Read more</span></a>
+                    <a onclick="openPopup()"><span>Read more</span></a>
                 </div>
             </div>
 
@@ -158,7 +158,7 @@
                     </div>
                 </div>
                 <div class="read-more-btn">
-                    <a><span>Read more</span></a>
+                    <a onclick="openPopup()"><span>Read more</span></a>
                 </div>
             </div>
 
@@ -183,7 +183,7 @@
                     </div>
                 </div>
                 <div class="read-more-btn">
-                    <a><span>Read more</span></a>
+                    <a onclick="openPopup()"><span>Read more</span></a>
                 </div>
             </div>
 
@@ -191,13 +191,162 @@
       </div>
        <!--slider end-->
     </div>
+    </div>
+      <!--slider nav-->
+      <div class="slider-nav">
       <div class="swiper-button-next"></div>
       <div class="swiper-button-prev"></div>
-  </div>
+  </div>  
+    
 </div>
-   
 
+<!--Modals -->
+<div class="modal-container">
+    <!--Modals start -->
+    <div class="modal-box ghana-bg">
+    <div class="modal-content">
+        <a onclick="closePopup()" class="modal-close-btn"><i class="uil uil-times "></i> <span>Close</span></a>
+        <div class="modal-inner">
+            <h1 class="modal-bg-title">Ghana</h1>
+        </div>
+        <div class="modal-image-area">
+            <img src="../images/elmina.jpg" alt="" class="modal-img">
+        </div>
+        <div class="modal-text-content">
+            <h1 class="modal-title">Ghana</h1>
+            <div class="modal-title-sub">
+                <span>Country</span>
+                <h4>Africa</h4>
+            </div>
+            <p>Ghana,officially the Republic of Ghana, is a country in West Africa. It abuts the Gulf of Guinea and the Atlantic Ocean to the south, sharing a border with Ivory Coast in the west, Burkina Faso in the north, and Togo in the east. Ghana covers an area of 239,567 km2 (92,497 sq mi), spanning diverse biomes that range from coastal savannas to tropical rainforests. With over 32 million inhabitants, Ghana is the second-most populous country in West Africa. The capital and largest city is Accra; other significant cities include Kumasi, Tamale, and Sekondi-Takoradi.</p>
+        </div>
+    </div>
+    </div>
+    <!--Modals end -->
+    <!--Modals start -->
+    <div class="modal-box naija-bg">
+    <div class="modal-content">
+        <a class="modal-close-btn" onclick="closePopup()"><i class="uil uil-times "></i> <span>Close</span></a>
+        <div class="modal-inner">
+            <h1 class="modal-bg-title">Nigeria</h1>
+        </div>
+        <div class="modal-image-area">
+            <img src="../images/naija.jpg" alt="" class="modal-img">
+        </div>
+        <div class="modal-text-content">
+            <h1 class="modal-title">Nigeria</h1>
+            <div class="modal-title-sub">
+                <span>Country</span>
+                <h4>Africa</h4>
+            </div>
+            <p>Nigeria is a country of contrasts, where ancient traditions coexist with modern aspirations. Whether you want to explore the bustling city of Lagos, the sacred forest of Oshogbo, or the artistic treasures of Nike Art Gallery, Nigeria offers something for every traveler. You can also witness the diversity of Nigeria's cultures and religions, from the Muslim north to the Christian south, and enjoy the rich cuisine, music, and festivals that celebrate their heritage. Nigeria is not without its challenges, but it is also a country of resilience, creativity, and optimism, where you can experience the vitality of Africa.</p>
+        </div>
+    </div>
+    </div>
+    <!--Modals end -->
+
+    <!--Modals start -->
+    <div class="modal-box mauritius-bg">
+    <div class="modal-content">
+        <a class="modal-close-btn" onclick="closePopup()"><i class="uil uil-times "></i> <span>Close</span></a>
+        <div class="modal-inner">
+            <h1 class="modal-bg-title">Mauritius</h1>
+        </div>
+        <div class="modal-image-area">
+            <img src="../images/maur.jpg" alt="" class="modal-img">
+        </div>
+        <div class="modal-text-content">
+            <h1 class="modal-title">Mauritius</h1>
+            <div class="modal-title-sub">
+                <span>Country</span>
+                <h4>Africa</h4>
+            </div>
+            <p>Mauritius,officially the Republic of Mauritius,[b] is an island country in the Indian Ocean, about 2,000 kilometres (1,100 nautical miles) off the southeastern coast of East Africa, east of Madagascar. It includes the main island (also called Mauritius), as well as Rodrigues, Agaléga, and St. Brandon (Cargados Carajos shoals).[12][13] The island of Mauritius along with nearby Réunion (a French overseas department), are part of the Mascarene Islands. The main island of Mauritius, where the population is concentrated, hosts the capital and largest city, Port Louis. The country spans 2,040 square kilometres (790 sq mi) and has an exclusive economic zone covering 2,300,000 square kilometres (670,000 square nautical miles).</p>
+        </div>
+    </div>
+    </div>
+    <!--Modals end -->
+
+    <!--Modals start -->
+    <div class="modal-box italy-bg">
+    <div class="modal-content">
+        <a class="modal-close-btn" onclick="closePopup()"><i class="uil uil-times "></i> <span>Close</span></a>
+        <div class="modal-inner">
+            <h1 class="modal-bg-title">Italy</h1>
+        </div>
+        <div class="modal-image-area">
+            <img src="../images/italypic.jpg" alt="" class="modal-img">
+        </div>
+        <div class="modal-text-content">
+            <h1 class="modal-title">Italy</h1>
+            <div class="modal-title-sub">
+                <span>Country</span>
+                <h4>Europe</h4>
+            </div>
+            <p>Italy is a dream destination for travelers who love art, culture, and cuisine. Whether you want to admire the ancient wonders of Rome, cruise the romantic canals of Venice, or explore the fashion capital of Milan, Italy has something for everyone. You can also enjoy the stunning scenery of the Amalfi Coast, the rolling hills of Tuscany, or the snowy peaks of the Alps. Italy is a feast for the senses, with its rich history, vibrant colors, and delicious flavors. Experience the dolce vita in one of the most beautiful and diverse countries in Europe.</p>
+        </div>
+    </div>
+    </div>
+    <!--Modals end -->
+
+    <!--Modals start -->
+    <div class="modal-box france-bg">
+    <div class="modal-content">
+        <a class="modal-close-btn" onclick="closePopup()"><i class="uil uil-times "></i> <span>Close</span></a>
+        <div class="modal-inner">
+            <h1 class="modal-bg-title">France</h1>
+        </div>
+        <div class="modal-image-area">
+            <img src="../images/paris.jpg" alt="" class="modal-img">
+        </div>
+        <div class="modal-text-content">
+            <h1 class="modal-title">France</h1>
+            <div class="modal-title-sub">
+                <span>Country</span>
+                <h4>Europe</h4>
+            </div>
+            <p>France is a country that charms travelers with its diverse and captivating attractions. Whether you are drawn to the iconic Eiffel Tower, the world-class art of the Centre Pompidou, or the romantic cafe terraces, you will find something to love in France. Explore the rich history and culture of its cities, from Paris to Marseille, or discover the stunning landscapes and villages of its countryside, from Provence to Normandy. France offers a variety of experiences for every taste and budget, and is a destination that will seduce you with its beauty and elegance.</p>
+        </div>
+    </div>
+    </div>
+    <!--Modals end -->
+
+
+    <!--Modals start -->
+    <div class="modal-box barb-bg">
+    <div class="modal-content">
+        <a class="modal-close-btn" onclick="closePopup()"><i class="uil uil-times "></i> <span>Close</span></a>
+        <div class="modal-inner">
+            <h1 class="modal-bg-title">Barbados</h1>
+        </div>
+        <div class="modal-image-area">
+            <img src="../images/barb.jpg" alt="" class="modal-img">
+        </div>
+        <div class="modal-text-content">
+            <h1 class="modal-title">Barbados</h1>
+            <div class="modal-title-sub">
+                <span>Country</span>
+                <h4>N.America</h4>
+            </div>
+            <p>Barbados is a gem of the Caribbean, where you can experience the best of both worlds: the laid-back beach life and the lively cultural scene. Whether you want to soak up the sun on the white-sand shores, dive into the turquoise waters, or explore the historic sites and natural wonders, Barbados has something for you. You can taste the island's rich culinary heritage, from the spicy fish cakes to the world-famous rum, and enjoy the music, art, and festivals that showcase the Bajan spirit. Barbados is more than just a destination, it's a way of life</p>
+        </div>
+    </div>
+    </div>
+    <!--Modals end -->
+
+
+
+    
+</div>
+
+   
+</section>
 <script src="../js/swiper-bundle.min.js"></script>
 <script src="../js/java.js"></script>
+
+<script>
+   
+    
+</script>
 </body>
 </html>
