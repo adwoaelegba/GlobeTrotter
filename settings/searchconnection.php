c2cbbec59a2a8d22710910f7643a01dbcb3ab667
adwoaelegba <naelegba@gmail.com>
2024-04-13 14:47:21 +0000

    Database server for pass

--- settings/searchconnection.php
+++ settings/searchconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "travel" );
+$con = mysqli_connect("localhost", "root", "JJLXSURT/2Yn", "travel" );
 
 if(!$con){
     echo "Connection failed" . mysqli_connect_error();
